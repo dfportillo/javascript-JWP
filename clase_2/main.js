e200592a92cd47fd0eddaaa6fc7657072984cec9
daniel portillo <danielportillo303@gmail.com>
2022-09-24 09:43:56 -0300

    se agregaron funciones para agregar y borrar elementos del carrito

--- clase_2/main.js
+++ clase_2/main.js
@@ -1,14 +1,38 @@
 // estructura de carrito
 
-const carrito = new Map();
 
-btnAgregar.addEventListener("click",() => {
-    const nombreProducto = inputProductos.value
-    carrito.set(nombreProducto,0)
-})
+btnAgregar.addEventListener("click",
+    agregarProductoCarrito 
+)
 
-function renderizarLista (){
+document.addEventListener("keyup", (e) => {
+    if(e.key === "Enter"){
+        agregarProductoCarrito()
+    }
+} )
+
+function agregarProductoCarrito () {
+    const nombreProducto = inputProductos.value;
+    carrito.set(nombreProducto, 1);
+    renderizarLista();
+    inputProductos.value = ''
+}
+
+function renderizarLista() {
+    listaProductos.replaceChildren(); //? sirve para cambiar a los hijos del "ul"
     for (let i of carrito.keys()) {
-        
+        const liEl = document.createElement("li");
+        liEl.classList.add("mdl-list__item");
+        const liElSpan = document.createElement("span");
+        liElSpan.classList.add("mdl-list__item-primary-content");
+        liElSpan.innerText = i;
+        liEl.appendChild(liElSpan);
+        listaProductos.appendChild(liEl);
     }
 }
+
+btnBorrar.addEventListener ("click", () => {
+    carrito.clear();
+    renderizarLista ();
+}
+)
--- clase_2/variables.js
+++ clase_2/variables.js
@@ -1,3 +1,5 @@
-const btnAgregar = document.querySelector("#btnAgregar")
-const inputProductos = document.querySelector("#sample1")
-const listaProductos = document.querySelector("#listProductos")
+const btnAgregar = document.querySelector("#btnAgregar");
+const btnBorrar = document.querySelector("#btnBorrar")
+const inputProductos = document.querySelector("#sample1");
+const listaProductos = document.querySelector("#listProductos");
+const carrito = new Map();
